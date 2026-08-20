4ca84a700119fdafc9099e90a527aad7ca3a8f51
Ranygila <gilarany@gmail.com>
2026-08-20 20:49:52 -0300

    Fix variable declaration and logging in script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ console.log(botaoBaixarCurriculo)
 botaoBaixarCurriculo.addEventListener("click", function() {
     console.log("Botão clicado!")
 
-let.tagA = document.createElement("a")
+let tagA = document.createElement("a")
 CONSOLE.log(tagA)
-TagA.href = caminhoCurriculo
+tagA.href = caminhoCurriculo
 tagA.download = "VITOR_MOUTIM_PT-BR.pdf"
 tagA.click()
 
