5a04e54e7b22ac3972cd0a9dd5b9bb6b8d9859ca
Ranygila <gilarany@gmail.com>
2026-08-20 20:46:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let caminhoCurriculo = "./VITOR_MOUTIM_PT-BR.pdf"
+
+let botaoBaixarCurriculo = document.querySelector("#botao-curriculo")
+console.log(botaoBaixarCurriculo)
+
+botaoBaixarCurriculo.addEventListener("click", function() {
+    console.log("Botão clicado!")
+
+let.tagA = document.createElement("a")
+CONSOLE.log(tagA)
+TagA.href = caminhoCurriculo
+tagA.download = "VITOR_MOUTIM_PT-BR.pdf"
+tagA.click()
+
+})
